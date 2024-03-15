ba168fb8549bae8a138f244d9426318c6beda86f
Sergey Stupak <itstupak@gmail.com>
2024-03-15 12:51:27 +0100

    Update comment in task-3

--- js/task-3.js
+++ js/task-3.js
@@ -16,6 +16,14 @@ function getElementWidth(content, padding, border) {
 
     //Працюють всі, але який правильніший?:)
 }
+//Ще спробував записати функцію інакше і теж працює:
+
+/* function getElementWidth(content, padding, border) {
+    content = Number.parseFloat(content);
+    padding = Number.parseFloat(padding);
+    border = Number.parseFloat(border);
+    return (content + 2 * padding + 2 * border);
+} */
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
